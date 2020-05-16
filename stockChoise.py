3be884fe35d8db3b89200907238af2f50bd96815
challengezcy <challengezcy@163.com>
2020-05-16 20:32:47 +0800

    tzlife: force pep8 format
    Using pycharm to force pep8 format.

--- stockChoise.py
+++ stockChoise.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 # 20200402 修正
 # 原因：2020年1到3月份由于新冠疫情导致“垃圾股”的单边下跌，无力反弹，特加入一些基本面的参考因素
 
 __author__ = "challengezcy@163.com"
 
-#python stock_choice.py
-
-#pip install tushare
+# pip install tushare
 import tushare as ts
 import pandas as pd
 import platform
@@ -16,20 +14,20 @@ from sys import argv
 import os
 import datetime
 
-#数据D=日k线 W=周 M=月 5=5分钟 15=15分钟 30=30分钟 60=60分钟
+# 数据D=日k线 W=周 M=月 5=5分钟 15=15分钟 30=30分钟 60=60分钟
 
-#python stockChoise.py W
+# python stockChoise.py W
 _, period = argv
 
 
-DAYNUM = 22 #过去22天的交易数据
-EXPECT = 13 #其中有13次涨跌幅超过5%
-NOT_GOOD = 16 #涨跌幅超过5%的有16次，认为这个股票不适合
-NO_DIFF = 3 #涨的次数和跌的次数差的绝对值
+DAYNUM = 22  # 过去22天的交易数据
+EXPECT = 13  # 其中有13次涨跌幅超过5%
+NOT_GOOD = 16  # 涨跌幅超过5%的有16次，认为这个股票不适合
+NO_DIFF = 3  # 涨的次数和跌的次数差的绝对值
 
 FLUC_INFO = []
 
-#过去n年的报表
+# 过去n年的报表
 NYEAR = 4
 
 '''ts.get_stock_basics()
@@ -49,64 +47,70 @@ ts.get_cashflow_data(2015,1)
 
 '''ts.get_report_data(年份，季度）, 可以获取每一年的季度的业绩。
    如果想要获取上市公司的年报，只要把季度参数改为4即可'''
+
+
 def load_report_datas(year, repfile):
-	if (os.path.exists(repfile)):
-		report = pd.read_csv(repfile)
-	else:
-		try:
-			report = ts.get_report_data(year, 4)
-		except Exception as e:
-			print(e)
+    if (os.path.exists(repfile)):
+        report = pd.read_csv(repfile)
+    else:
+        try:
+            report = ts.get_report_data(year, 4)
+        except Exception as e:
+            print(e)
 
-	if (not os.path.exists(repfile)):
-		report.fillna(0)
-		report.to_csv(repfile)
-		report = pd.read_csv(repfile)
+    if (not os.path.exists(repfile)):
+        report.fillna(0)
+        report.to_csv(repfile)
+        report = pd.read_csv(repfile)
 
-	return report
+    return report
+
 
 def get_n_year_report():
-	year = datetime.datetime.now().year
-	repny = {}
-	i = 1
-	while i <= NYEAR:
-		year = year - 1
-		repfile = 'report_' + str(year) + '.csv'
-		rep = load_report_datas(year, repfile)
-		repny[year] = rep
-		i = i + 1
-	return repny
+    year = datetime.datetime.now().year
+    repny = {}
+    i = 1
+    while i <= NYEAR:
+        year = year - 1
+        repfile = 'report_' + str(year) + '.csv'
+        rep = load_report_datas(year, repfile)
+        repny[year] = rep
+        i = i + 1
+    return repny
+
 
 def load_all_stock_basics():
-	basic = pd.read_csv("basic.csv")
-	basic.set_index(['code'], inplace = True)
+    basic = pd.read_csv("basic.csv")
+    basic.set_index(['code'], inplace=True)
 
-	return basic
+    return basic
+
 
 def load_today_all_stocks():
-	try:
-		df = ts.get_stock_basics()
-	except Exception as e:
-		print(e)
-		df = pd.read_csv("basic.csv")
+    try:
+        df = ts.get_stock_basics()
+    except Exception as e:
+        print(e)
+        df = pd.read_csv("basic.csv")
 
-	if (os.path.exists("basic.csv")):
-		os.remove("basic.csv")
+    if (os.path.exists("basic.csv")):
+        os.remove("basic.csv")
 
-	df.to_csv("basic.csv")
+    df.to_csv("basic.csv")
 
-	allcode = []
-	df = pd.read_csv("basic.csv")
-	df.set_index(['code'], inplace = True)
-	for i in df.index:
-		i = "%06d" % i
-		#i = i.zfill(6) 文本型补零
-		allcode.append(i)
-		#print(i)
-	return allcode
+    allcode = []
+    df = pd.read_csv("basic.csv")
+    df.set_index(['code'], inplace=True)
+    for i in df.index:
+        i = "%06d" % i
+        # i = i.zfill(6) 文本型补零
+        allcode.append(i)
+        # print(i)
+    return allcode
 
-#---------------------------------------
-#For debug use
+
+# ---------------------------------------
+# For debug use
 '''
 def calculate_stock_fluctuation_HZ1(stock):
 	stock_item = {}
@@ -114,129 +118,129 @@ def calculate_stock_fluctuation_HZ1(stock):
 	stock_item['score'] = 0
 	return stock_item
 '''
-#---------------------------------------
+# ---------------------------------------
+
 
 def calculate_stock_fluctuation_HZ(stock):
-	global period
-	if str(period) == 'D':
-		wave = 0.06
-	elif str(period) == 'W':
-		wave = 0.10
-	else:
-		print('The parameter "Period" is not right')
-		period = 'D'
-		wave = 0.06
+    global period
+    if str(period) == 'D':
+        wave = 0.06
+    elif str(period) == 'W':
+        wave = 0.10
+    else:
+        print('The parameter "Period" is not right')
+        period = 'D'
+        wave = 0.06
 
-	data = ts.get_k_data(stock, ktype=str(period), autype='qfq')
-	try:
-		data = data.sort_values(by='date', ascending=False)
-		data = data.set_index('date')
-	except KeyError:
-		return 0
+    data = ts.get_k_data(stock, ktype=str(period), autype='qfq')
+    try:
+        data = data.sort_values(by='date', ascending=False)
+        data = data.set_index('date')
+    except KeyError:
+        return 0
 
-	if data.shape[0] < DAYNUM + 5:
-		return 0
+    if data.shape[0] < DAYNUM + 5:
+        return 0
 
-	stock_item = {}
-	stock_item['stock'] = stock.zfill(6)
-	for j in (1,): #(1,2,3,5)
-		fluc_up = 0
-		fluc_dw = 0
-		fluc = 0
-		for i in range(DAYNUM, -1, -1):
-			if data.close[i+j] < data.high[i] and round((data.high[i] - data.close[i+j])/(data.close[i+j]),2) > wave:
-				fluc_up = fluc_up + 1
-			if data.close[i+j] > data.low[i] and round((data.close[i+j] - data.low[i])/(data.close[i+j]),2) > wave:
-				fluc_dw = fluc_dw + 1
-		stock_item['aT'] = fluc_up + fluc_dw
-		stock_item['d'] = fluc_up - fluc_dw
-		#stock_item['aup'] = fluc_up
-		#stock_item['adw'] = fluc_dw
-		stock_item['p'] = data.close[i]
-		if (stock_item['aT'] < EXPECT or stock_item['aT'] > NOT_GOOD or stock_item['d'] > NO_DIFF):
-			return 0
-		stock_item['score'] = 0
+    stock_item = {}
+    stock_item['stock'] = stock.zfill(6)
+    for j in (1,):  # (1,2,3,5)
+        fluc_up = 0
+        fluc_dw = 0
+        fluc = 0
+        for i in range(DAYNUM, -1, -1):
+            if data.close[i + j] < data.high[i] and round(
+                    (data.high[i] - data.close[i + j]) / (data.close[i + j]), 2) > wave:
+                fluc_up = fluc_up + 1
+            if data.close[i + j] > data.low[i] and round(
+                    (data.close[i + j] - data.low[i]) / (data.close[i + j]), 2) > wave:
+                fluc_dw = fluc_dw + 1
+        stock_item['aT'] = fluc_up + fluc_dw
+        stock_item['d'] = fluc_up - fluc_dw
+        # stock_item['aup'] = fluc_up
+        # stock_item['adw'] = fluc_dw
+        stock_item['p'] = data.close[i]
+        if (stock_item['aT'] < EXPECT or stock_item['aT']
+                > NOT_GOOD or stock_item['d'] > NO_DIFF):
+            return 0
+        stock_item['score'] = 0
 
-	return stock_item
+    return stock_item
+
 
 def get_specific_stock_basic(scode, item, basics):
-	scode = int(scode)
-	name = basics.loc[scode, 'name']
-	industry = basics.loc[scode, 'industry']
-	area = basics.loc[scode, 'area']
-	pe = basics.loc[scode, 'pe']
-	pb = basics.loc[scode, 'pb']
-	liquidasset = basics.loc[scode, 'liquidAssets']
-	fixedasset = basics.loc[scode, 'fixedAssets']
-	totalasset = basics.loc[scode, 'totalAssets']
-	outstanding = basics.loc[scode, 'outstanding']
-	totals = basics.loc[scode, 'totals']
-	esp = basics.loc[scode, 'esp']
-	bvps = basics.loc[scode, 'bvps']
-	reservedpershare = basics.loc[scode, 'reservedPerShare']
-	perundp = basics.loc[scode, 'perundp']
+    scode = int(scode)
+    name = basics.loc[scode, 'name']
+    industry = basics.loc[scode, 'industry']
+    area = basics.loc[scode, 'area']
+    pe = basics.loc[scode, 'pe']
+    pb = basics.loc[scode, 'pb']
+    liquidasset = basics.loc[scode, 'liquidAssets']
+    fixedasset = basics.loc[scode, 'fixedAssets']
+    totalasset = basics.loc[scode, 'totalAssets']
+    outstanding = basics.loc[scode, 'outstanding']
+    totals = basics.loc[scode, 'totals']
+    esp = basics.loc[scode, 'esp']
+    bvps = basics.loc[scode, 'bvps']
+    reservedpershare = basics.loc[scode, 'reservedPerShare']
+    perundp = basics.loc[scode, 'perundp']
 
-	item['pe'] = pe
-	item['pb'] = pb
-	item['zIdustry'] = str(industry)
+    item['pe'] = pe
+    item['pb'] = pb
+    item['zIdustry'] = str(industry)
+
 
 def get_specific_stock_extend(scode, item, reports):
-	year = datetime.datetime.now().year - 1
-	for i in range(0, NYEAR):
-		report = reports[year]
-		tr = 'roe' + str(year-2000)
+    year = datetime.datetime.now().year - 1
+    for i in range(0, NYEAR):
+        report = reports[year]
+        tr = 'roe' + str(year - 2000)
 
-		year = year - 1
-		tre = report[report['code'] == int(scode)]
-		if tre.empty:
-			item['score'] = item['score'] + 1 #No roe, +1
-			item[tr] = -99
-		else:
-			tmp = tre.to_dict(orient='list')
-			item[tr] = tmp['roe'][0]
-			if tmp['roe'][0] > 50:
-				item['score'] = item['score'] + 2
-			elif tmp['roe'][0] > 20:
-				item['score'] = item['score'] + 5
-			elif tmp['roe'][0] > 10:
-				item['score'] = item['score'] + 3
-			elif tmp['roe'][0] > 0:
-				item['score'] = item['score'] + 1
-			else:
-				item['score'] = item['score'] + 0
+        year = year - 1
+        tre = report[report['code'] == int(scode)]
+        if tre.empty:
+            item['score'] = item['score'] + 1  # No roe, +1
+            item[tr] = -99
+        else:
+            tmp = tre.to_dict(orient='list')
+            item[tr] = tmp['roe'][0]
+            if tmp['roe'][0] > 50:
+                item['score'] = item['score'] + 2
+            elif tmp['roe'][0] > 20:
+                item['score'] = item['score'] + 5
+            elif tmp['roe'][0] > 10:
+                item['score'] = item['score'] + 3
+            elif tmp['roe'][0] > 0:
+                item['score'] = item['score'] + 1
+            else:
+                item['score'] = item['score'] + 0
+
 
 if __name__ == '__main__':
-	if platform.system == 'Windows':
-		cmd = 'chcp 936'
-		os.system(cmd)
+    if platform.system == 'Windows':
+        cmd = 'chcp 936'
+        os.system(cmd)
 
-	if (os.path.exists("getItFun.csv")):
-		os.remove("getItFun.csv")
+    if (os.path.exists("getItFun.csv")):
+        os.remove("getItFun.csv")
 
-	all_code = load_today_all_stocks()
-	stock_basics = load_all_stock_basics()
-	report_data = get_n_year_report()
+    all_code = load_today_all_stocks()
+    stock_basics = load_all_stock_basics()
+    report_data = get_n_year_report()
 
-	i = 0
-	for stock in all_code:
-		if (i>30):
-			break;
-		item = calculate_stock_fluctuation_HZ(stock)
-		if item != 0:
-			get_specific_stock_basic(stock, item, stock_basics)
-			get_specific_stock_extend(stock, item, report_data)
-			FLUC_INFO.append(item)
-		i = i + 1
-		print('%d' % i)
+    i = 0
+    for stock in all_code:
+        item = calculate_stock_fluctuation_HZ(stock)
+        if item != 0:
+            get_specific_stock_basic(stock, item, stock_basics)
+            get_specific_stock_extend(stock, item, report_data)
+            FLUC_INFO.append(item)
+        i = i + 1
+        print('%d' % i)
 
-	df = pd.DataFrame(FLUC_INFO)
-	df.set_index(['stock'], inplace=True)
-	df.sort_values(by=['score'], axis=0, ascending=False, inplace=True)
-	print(df)
-	df.to_csv("getItFun.csv")
-	print("Let's have fun!")
-
-	
-	
-	
-
+    df = pd.DataFrame(FLUC_INFO)
+    df.set_index(['stock'], inplace=True)
+    df.sort_values(by=['score'], axis=0, ascending=False, inplace=True)
+    print(df)
+    df.to_csv("getItFun.csv")
+    print("Let's have fun!")
--- wanggeJY.py
+++ wanggeJY.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env phython3
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
-#20200406 修正 -- version2
+# 20200406 修正 -- version2
 #    原因：2020年1到4月，因新冠疫情导致的极端下跌行情
 #    修正：当买入4个档位后，后面买入的档位在前一个的基础上+1.5%
 #    说明：第5格买入之后，市场／个股可能已经出现极端情况，从第6格开始更多的防守，保证资金安全为前提
 
 __author__ = "challengezcy@163.com"
 
-#说明：
-#为了资金安全，触发价通常会选择相对低价，如60日线均价再下调10%～20%
-#这时候，高于触发价的网格买入规则，第二格或者第三格作为第一次手动买入
+# 说明：
+# 为了资金安全，触发价通常会选择相对低价，如60日线均价再下调10%～20%
+# 这时候，高于触发价的网格买入规则，第二格或者第三格作为第一次手动买入
 
 from openpyxl import Workbook
 from sys import argv
@@ -19,188 +19,244 @@ import os
 
 #price: 第二档买入价
 #Trade_mount: 此次网格交易总共投入的实际资金
-#load: 相当于加多少杠杆交易（0～100）
+# load: 相当于加多少杠杆交易（0～100）
 
-#python wanggeJY.py 18.79 200000 10
+# python wanggeJY.py 18.79 200000 10
 _, price, Trade_mount, loan = argv
 
-#默认10个网格
+# 默认10个网格
 GRID_COUNT = 10
 UP_BASE_PRICE_GRID_COUNT = 4
 
-#默认涨6%卖出
+# 默认涨6%卖出
 UP_range = 6
 up_range = (int)(UP_range)
 
-#默认跌6%买入，参考20200406修正说明
+# 默认跌6%买入，参考20200406修正说明
 down_range = up_range
 
-#base_price: 基准买入价，用来计算每个网格的买入量
-base_price = ((float)(price))*((100-(int)(down_range))/100)**3
+# base_price: 基准买入价，用来计算每个网格的买入量
+base_price = ((float)(price)) * ((100 - (int)(down_range)) / 100)**3
 
-#不加借贷的资金额
+# 不加借贷的资金额
 Trade_mount = (int)(Trade_mount)
 init_mount = Trade_mount
 
-#加借贷后的资金额
+# 加借贷后的资金额
 loan = (int)(loan)
-Trade_mount = Trade_mount*(100 + loan)/100
+Trade_mount = Trade_mount * (100 + loan) / 100
 
-#以base_price为基准，平均每个grid可以买到多少股
-#买入股票数是100的整数倍
-Trade_count = round((((float)(Trade_mount))/((float)(base_price)))/(GRID_COUNT*100), 0) * 100
+# 以base_price为基准，平均每个grid可以买到多少股
+# 买入股票数是100的整数倍
+Trade_count = round(
+    (((float)(Trade_mount)) / ((float)(base_price))) / (GRID_COUNT * 100), 0) * 100
 trade_count = (int)(Trade_count)
 
 if (os.path.exists("op_plan.xlsx")):
-	os.remove("op_plan.xlsx")
+    os.remove("op_plan.xlsx")
 
-#根据当日的收盘价，计算下一个交易日的涨／跌停价
+# 根据当日的收盘价，计算下一个交易日的涨／跌停价
 #close = round((float)(Close), 2)
 #close_up = close + round(close*10/100, 2)
 #close_down = close - round(close*10/100, 2)
 #print("close up %f, close down %f" % (close_up, close_down))
 
-#建立excel表格
+# 建立excel表格
 wb = Workbook()
 ws = wb.active
 ws.title = "OP_PLAN"
 ws.sheet_properties.tabColor = "ff0033"
 
+
 def generate_base_grid():
-	for i in range(1, GRID_COUNT + 1):
-		if i <= UP_BASE_PRICE_GRID_COUNT:
-			if i == 1:
-				ws.cell(row=i, column=1).value = ((float)(price))*((100+(int)(up_range))/100)
-				ws.cell(row=i, column=2).value = ws.cell(row=i, column=1).value * ((100+(int)(up_range))/100)
-			elif i == 2:
-				ws.cell(row=i, column=1).value = ((float)(price))
-				ws.cell(row=i, column=2).value = ws.cell(row=i, column=1).value * ((100+(int)(up_range))/100)
-			else:
-				ref_price = ws.cell(row=i-1, column=1).value
-				ws.cell(row=i, column=1).value = ((float)(ref_price))*((100-(int)(down_range))/100)
-				ws.cell(row=i, column=2).value = (float)(ref_price)
-			ws.cell(row=i, column=1).value = round(ws.cell(row=i, column=1).value, 2)
-			ws.cell(row=i, column=2).value = round(ws.cell(row=i, column=2).value, 2)
+    for i in range(1, GRID_COUNT + 1):
+        if i <= UP_BASE_PRICE_GRID_COUNT:
+            if i == 1:
+                ws.cell(row=i, column=1).value = ((float)(price)) * \
+                    ((100 + (int)(up_range)) / 100)
+                ws.cell(row=i, column=2).value = ws.cell(
+                    row=i, column=1).value * ((100 + (int)(up_range)) / 100)
+            elif i == 2:
+                ws.cell(row=i, column=1).value = ((float)(price))
+                ws.cell(row=i, column=2).value = ws.cell(
+                    row=i, column=1).value * ((100 + (int)(up_range)) / 100)
+            else:
+                ref_price = ws.cell(row=i - 1, column=1).value
+                ws.cell(row=i, column=1).value = (
+                    (float)(ref_price)) * ((100 - (int)(down_range)) / 100)
+                ws.cell(row=i, column=2).value = (float)(ref_price)
+            ws.cell(
+                row=i,
+                column=1).value = round(
+                ws.cell(
+                    row=i,
+                    column=1).value,
+                2)
+            ws.cell(
+                row=i,
+                column=2).value = round(
+                ws.cell(
+                    row=i,
+                    column=2).value,
+                2)
 
-			#对低价股调整有意义，根据实际情况看是否需要调整
-			ws.cell(row=i, column=3).value = trade_count - round((UP_BASE_PRICE_GRID_COUNT-i)/2, 0) * 100
-			ws.cell(row=i, column=4).value = ws.cell(row=i, column=3).value * ws.cell(row=i, column=1).value
-		else:
-			ref_price = ws.cell(row=i-1, column=1).value
-			ws.cell(row=i, column=1).value = ((float)(ref_price))*((1000-(int)(10*down_range+(15*(i-UP_BASE_PRICE_GRID_COUNT))))/1000)
-			ws.cell(row=i, column=2).value = ws.cell(row=i, column=1).value * ((10000+(int)(up_range*100 + (i-UP_BASE_PRICE_GRID_COUNT)*40))/10000)
-			ws.cell(row=i, column=1).value = round(ws.cell(row=i, column=1).value, 2)
-			ws.cell(row=i, column=2).value = round(ws.cell(row=i, column=2).value, 2)
+            # 对低价股调整有意义，根据实际情况看是否需要调整
+            ws.cell(row=i, column=3).value = trade_count - \
+                round((UP_BASE_PRICE_GRID_COUNT - i) / 2, 0) * 100
+            ws.cell(row=i, column=4).value = ws.cell(
+                row=i, column=3).value * ws.cell(row=i, column=1).value
+        else:
+            ref_price = ws.cell(row=i - 1, column=1).value
+            ws.cell(row=i, column=1).value = ((float)(ref_price)) * ((1000 - (int)
+                                                                      (10 * down_range + (15 * (i - UP_BASE_PRICE_GRID_COUNT)))) / 1000)
+            ws.cell(row=i, column=2).value = ws.cell(row=i, column=1).value * \
+                ((10000 + (int)(up_range * 100 + (i - UP_BASE_PRICE_GRID_COUNT) * 40)) / 10000)
+            ws.cell(
+                row=i,
+                column=1).value = round(
+                ws.cell(
+                    row=i,
+                    column=1).value,
+                2)
+            ws.cell(
+                row=i,
+                column=2).value = round(
+                ws.cell(
+                    row=i,
+                    column=2).value,
+                2)
 
-			#对低价股调整有意义，根据实际情况看是否需要调整
-			ws.cell(row=i, column=3).value = trade_count + (i - UP_BASE_PRICE_GRID_COUNT) * 100
-			ws.cell(row=i, column=4).value = ws.cell(row=i, column=3).value * ws.cell(row=i, column=1).value
+            # 对低价股调整有意义，根据实际情况看是否需要调整
+            ws.cell(row=i, column=3).value = trade_count + \
+                (i - UP_BASE_PRICE_GRID_COUNT) * 100
+            ws.cell(row=i, column=4).value = ws.cell(
+                row=i, column=3).value * ws.cell(row=i, column=1).value
+
 
 def money_efficient():
-	global Trade_mount
-	Amount_money = 0
-	Amount_stock = 0
-	for i in range(1, GRID_COUNT+1):
-		Amount_money = Amount_money + ws.cell(row=i, column=4).value
-		Amount_stock = Amount_stock + ws.cell(row=i, column=3).value
+    global Trade_mount
+    Amount_money = 0
+    Amount_stock = 0
+    for i in range(1, GRID_COUNT + 1):
+        Amount_money = Amount_money + ws.cell(row=i, column=4).value
+        Amount_stock = Amount_stock + ws.cell(row=i, column=3).value
 
-	Trade_mount = (float)(Trade_mount)
-	if (Amount_money > Trade_mount):
-		diff_money = Amount_money - Trade_mount
-		for i in range(1, GRID_COUNT+1):
-			if (ws.cell(row=i, column=3).value == 0):
-				continue
-			tmp_money = ws.cell(row=i, column=1).value * 100
+    Trade_mount = (float)(Trade_mount)
+    if (Amount_money > Trade_mount):
+        diff_money = Amount_money - Trade_mount
+        for i in range(1, GRID_COUNT + 1):
+            if (ws.cell(row=i, column=3).value == 0):
+                continue
+            tmp_money = ws.cell(row=i, column=1).value * 100
 
-			ws.cell(row=i, column=3).value = ws.cell(row=i, column=3).value - 100
-			ws.cell(row=i, column=4).value = ws.cell(row=i, column=3).value * ws.cell(row=i, column=1).value
+            ws.cell(
+                row=i, column=3).value = ws.cell(
+                row=i, column=3).value - 100
+            ws.cell(row=i, column=4).value = ws.cell(
+                row=i, column=3).value * ws.cell(row=i, column=1).value
 
-			if (tmp_money < diff_money):
-				diff_money = diff_money - tmp_money
-			else:
-				break
+            if (tmp_money < diff_money):
+                diff_money = diff_money - tmp_money
+            else:
+                break
 
-	if (Amount_money < Trade_mount):
-		diff_money = Trade_mount - Amount_money
-		for i in range(GRID_COUNT, 0, -1):
-			tmp_money = ws.cell(row=i, column=1).value * 100
+    if (Amount_money < Trade_mount):
+        diff_money = Trade_mount - Amount_money
+        for i in range(GRID_COUNT, 0, -1):
+            tmp_money = ws.cell(row=i, column=1).value * 100
 
-			if (tmp_money < diff_money):
-				ws.cell(row=i, column=3).value = ws.cell(row=i, column=3).value + 100
-				ws.cell(row=i, column=4).value = ws.cell(row=i, column=3).value * ws.cell(row=i, column=1).value
-				diff_money = diff_money - tmp_money
-			else:
-				break
+            if (tmp_money < diff_money):
+                ws.cell(
+                    row=i, column=3).value = ws.cell(
+                    row=i, column=3).value + 100
+                ws.cell(row=i, column=4).value = ws.cell(
+                    row=i, column=3).value * ws.cell(row=i, column=1).value
+                diff_money = diff_money - tmp_money
+            else:
+                break
+
 
 def money_balance_among_grid():
-	while True:
-		money_high = 0
-		money_low = 0
-		high_index = 0
-		low_index = 0
-		for i in range(1, GRID_COUNT+1):
-			if (i == 1):
-				money_high = ws.cell(row=i, column=4).value
-				money_low = ws.cell(row=i, column=4).value
-				high_index = i
-				low_index = i
-			else:
-				if (money_high < ws.cell(row=i, column=4).value):
-					money_high = ws.cell(row=i, column=4).value
-					high_index = i
-				if (money_low > ws.cell(row=i, column=4).value):
-					money_low = ws.cell(row=i, column=4).value
-					low_index = i
+    while True:
+        money_high = 0
+        money_low = 0
+        high_index = 0
+        low_index = 0
+        for i in range(1, GRID_COUNT + 1):
+            if (i == 1):
+                money_high = ws.cell(row=i, column=4).value
+                money_low = ws.cell(row=i, column=4).value
+                high_index = i
+                low_index = i
+            else:
+                if (money_high < ws.cell(row=i, column=4).value):
+                    money_high = ws.cell(row=i, column=4).value
+                    high_index = i
+                if (money_low > ws.cell(row=i, column=4).value):
+                    money_low = ws.cell(row=i, column=4).value
+                    low_index = i
 
-		if (money_high-money_low > ((float)(base_price) * 200)):
-			ws.cell(row=low_index, column=3).value = ws.cell(row=low_index, column=3).value + 100
-			ws.cell(row=low_index, column=4).value = ws.cell(row=low_index, column=3).value * ws.cell(row=low_index, column=1).value
-			ws.cell(row=high_index, column=3).value = ws.cell(row=high_index, column=3).value - 100
-			ws.cell(row=high_index, column=4).value = ws.cell(row=high_index, column=3).value * ws.cell(row=high_index, column=1).value
-		else:
-			break
+        if (money_high - money_low > ((float)(base_price) * 200)):
+            ws.cell(
+                row=low_index,
+                column=3).value = ws.cell(
+                row=low_index,
+                column=3).value + 100
+            ws.cell(row=low_index, column=4).value = ws.cell(
+                row=low_index, column=3).value * ws.cell(row=low_index, column=1).value
+            ws.cell(
+                row=high_index,
+                column=3).value = ws.cell(
+                row=high_index,
+                column=3).value - 100
+            ws.cell(row=high_index, column=4).value = ws.cell(
+                row=high_index, column=3).value * ws.cell(row=high_index, column=1).value
+        else:
+            break
+
 
 def stock_op_plan():
-	Amount_money = 0
-	Amount_stock = 0
-	for i in range(1, GRID_COUNT+1):
-		Amount_money = Amount_money + ws.cell(row=i, column=4).value
-		Amount_stock = Amount_stock + ws.cell(row=i, column=3).value
+    Amount_money = 0
+    Amount_stock = 0
+    for i in range(1, GRID_COUNT + 1):
+        Amount_money = Amount_money + ws.cell(row=i, column=4).value
+        Amount_stock = Amount_stock + ws.cell(row=i, column=3).value
 
-	ws.cell(row=GRID_COUNT+1, column=3).value = Amount_stock
-	ws.cell(row=GRID_COUNT+1, column=4).value = Amount_money
+    ws.cell(row=GRID_COUNT + 1, column=3).value = Amount_stock
+    ws.cell(row=GRID_COUNT + 1, column=4).value = Amount_money
 
-	for i in range(GRID_COUNT+1, 0, -1):
-		if i != GRID_COUNT+1:
-			ws.cell(row=i+1, column=1).value = ws.cell(row=i, column=1).value
-			ws.cell(row=i+1, column=2).value = ws.cell(row=i, column=2).value
-		ws.cell(row=i+1, column=3).value = ws.cell(row=i, column=3).value
-		ws.cell(row=i+1, column=4).value = ws.cell(row=i, column=4).value
+    for i in range(GRID_COUNT + 1, 0, -1):
+        if i != GRID_COUNT + 1:
+            ws.cell(row=i + 1, column=1).value = ws.cell(row=i, column=1).value
+            ws.cell(row=i + 1, column=2).value = ws.cell(row=i, column=2).value
+        ws.cell(row=i + 1, column=3).value = ws.cell(row=i, column=3).value
+        ws.cell(row=i + 1, column=4).value = ws.cell(row=i, column=4).value
 
-	ws.cell(row=1, column=1).value = 'Buy'
-	ws.cell(row=1, column=2).value = 'Sell'
-	ws.cell(row=1, column=3).value = 'Count'
-	ws.cell(row=1, column=4).value = 'Money'
+    ws.cell(row=1, column=1).value = 'Buy'
+    ws.cell(row=1, column=2).value = 'Sell'
+    ws.cell(row=1, column=3).value = 'Count'
+    ws.cell(row=1, column=4).value = 'Money'
 
-	ws.cell(row=GRID_COUNT+3, column=3).value = init_mount
-	ws.cell(row=GRID_COUNT+3, column=4).value = Trade_mount
-	ws.cell(row=GRID_COUNT+3, column=2).value = loan
+    ws.cell(row=GRID_COUNT + 3, column=3).value = init_mount
+    ws.cell(row=GRID_COUNT + 3, column=4).value = Trade_mount
+    ws.cell(row=GRID_COUNT + 3, column=2).value = loan
 
-	wb.save("op_plan.xlsx")
-	data_xls = pd.read_excel("op_plan.xlsx")
-	print(data_xls)
+    wb.save("op_plan.xlsx")
+    data_xls = pd.read_excel("op_plan.xlsx")
+    print(data_xls)
+
 
 if __name__ == '__main__':
-	generate_base_grid()
-	money_balance_among_grid()
-	for i in range(1, 10):
-		money_efficient()
-	money_balance_among_grid()
-	for i in range(1, 10):
-		money_efficient()
-	money_balance_among_grid()
-	#for i in range(1, 2):
-	#	money_efficient()
-	stock_op_plan()
+    generate_base_grid()
+    money_balance_among_grid()
+    for i in range(1, 10):
+        money_efficient()
+    money_balance_among_grid()
+    for i in range(1, 10):
+        money_efficient()
+    money_balance_among_grid()
+    # for i in range(1, 2):
+    #	money_efficient()
+    stock_op_plan()
 
-	print("Let's have fun!")
+    print("Let's have fun!")
